624b1240e101489dc26df521261ca0be887587cf
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2019-09-10 20:47:11 +0200

    Starting work on views and templates

--- rpguru/urls.py
+++ rpguru/urls.py
@@ -3,10 +3,12 @@
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='frontpage.html'), name='frontpage')
 ]
 
 if settings.DEBUG:
