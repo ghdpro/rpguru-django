fb791e7e87a596d69d8361f37d067ae223b531d1
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2019-09-18 12:04:16 +0200

    Further work on views and templates

--- rpguru/library/views.py
+++ rpguru/library/views.py
@@ -2,24 +2,69 @@
 
 from django.views.generic.detail import DetailView
 
-from .models import Platform, Franchise, Company, Genre
+from .models import Platform, Franchise, Company, Genre, Game
 
 
 class PlatformDetailView(DetailView):
     template_name = 'library/platform.html'
     model = Platform
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['games'] = Game.objects.filter(platform=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        return context
+
 
 class FranchiseDetailView(DetailView):
     template_name = 'library/franchise.html'
     model = Franchise
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Main games
+        context['main'] = Game.objects.filter(franchise_main=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        # Side games
+        context['side'] = Game.objects.filter(franchise_side=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        return context
 
 
 class CompanyDetailView(DetailView):
     template_name = 'library/company.html'
     model = Company
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Games developed
+        context['developed'] = Game.objects.filter(developer=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        # Games published
+        context['published'] = Game.objects.filter(publisher=context['object'])\
+            .exclude(developer=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        return context
+
 
 class GenreDetailView(DetailView):
     template_name = 'library/genre.html'
     model = Genre
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['games'] = Game.objects.filter(genre=context['object'])\
+            .select_related('franchise_main', 'franchise_side')\
+            .prefetch_related('audio', 'developer', 'publisher', 'genre', 'platform')\
+            .order_by('-na_date', '-jp_date', '-eu_date')
+        return context
