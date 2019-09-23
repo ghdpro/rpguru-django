4f6bf821c5bb8864420b133178c415f34abcc120
Gerard Krijgsman <2293043+ghdpro@users.noreply.github.com>
2019-09-23 11:10:51 +0200

    Finished Fabric script

--- fabfile.py
+++ fabfile.py
@@ -25,15 +25,18 @@ def deploy(c):
     rev = c.local('git rev-parse --short HEAD', timeout=1, hide=True).stdout.strip()
     _clone(c, rev)
     _venv(c, rev)
+    _generate_id(c, rev)
+    _backup_db(c)
     _migrate(c, rev)
     _update_symlinks(c, rev)
-    _generate_id(c, rev)
+    _reload(c)
 
 
 @task(hosts=hosts)
 def init(c):
     """Initializes project"""
     rev = 'current'
+    _backup_db(c)
     c.run(f'source {venv}/{rev}/bin/activate && cd {builds}'
           f'/{rev}/ && python3 manage.py migrate --settings=settings.staging', echo=True)
     c.run(f'source {venv}/{rev}/bin/activate && cd {builds}'
@@ -65,6 +68,15 @@ def _venv(c, rev):
     c.run(f'source {venv}/{rev}/bin/activate && pip install -Uq pip', echo=True)
     c.run(f'source {venv}/{rev}/bin/activate && pip install -Uq setuptools', echo=True)
     c.run(f'source {venv}/{rev}/bin/activate && pip install -Uqr {builds}/{rev}/requirements.txt', echo=True)
+
+
+def _backup_db(c):
+    # Make database backup before proceeding with operations that might mess with the database
+    c.run(f'mkdir -p {dump}', echo=True)
+    # This command assumes correctly configured .pgpass file is present
+    # Note that this database backup uses the custom format; use pg_restore to restore
+    c.run(f'cd {venv} && pg_dump -U rpguru -Fc rpguru > '
+          f'animesuki-{datetime.now().strftime("%Y-%m-%d-%H%M")}.dump', echo=True)
 
 
 def _migrate(c, rev):
@@ -99,3 +111,9 @@ def _generate_id(c, rev):
     output = f'Version {count.stdout.strip()}:{id.stdout.strip()} ({datetime.now().date().isoformat()})'
     c.local(f'echo "{output}" > {id_file}', timeout=1, echo=True)
     c.put(id_file, f'{builds}/{rev}/templates/')
+
+
+def _reload(c):
+    # Reload services - requires properly configured sudoers file
+    c.run('sudo service uwsgi reload', echo=True)
+    c.run('sudo service nginx reload', echo=True)
